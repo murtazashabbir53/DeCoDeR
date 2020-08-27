b894ccaee8082f66f60845883ee93b2d22966789
murtazashabbir53 <60929735+murtazashabbir53@users.noreply.github.com>
2020-08-27 15:46:41 +0530

    Create floatingnum.c

---
+++ Easy/floatingnum.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int T,i;
+      
+    float n;
+    scanf("%d",&T);
+    
+  
+    
+    
+    for(i=0;i<T;i++)
+    {scanf("%f",&n);
+            
+    
+    
+    
+        printf("%0.2f", n); 
+    }
+   return 0;
+}  
