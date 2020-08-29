5b88ac4b2f77cc29da334322e7966f269d3b41ab
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-08-29 19:30:01 +0530

    Create HappyString.c

---
+++ Easy/HappyString.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+
+{
+  int N;
+  
+  scanf("%d",&N);
+  for(N=96+N;N>=97;N--)
+    {
+    
+      printf("%c",N);
+    }
+   
+           
+   return 0;
+}
