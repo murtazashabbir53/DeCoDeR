6531845dde4b3ccf4b2fb35117712e628a14450d
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-08-29 16:10:41 +0530

    Create CircleNumPartner.c

---
+++ Easy/CircleNumPartner.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+
+{
+  int N,X,prtner;
+  scanf("%d %d",&N,&X);
+  
+  
+  prtner = X+4;
+  printf("%d",prtner);
+ 
+  
+  return 0;
+}
