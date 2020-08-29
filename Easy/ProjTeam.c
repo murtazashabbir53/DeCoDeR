e5d45aad0a6e38a7cc6cdcc0438d47b43f338984
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-08-29 14:59:04 +0530

    Create ProjTeam.c

---
+++ Easy/ProjTeam.c
@@ -0,0 +1,15 @@
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+
+int main()
+{int N,grps;
+    
+    scanf("%d",&N);
+    grps = floor(N/3);
+    printf("%d",grps);
+   
+      
+    
+    return 0;
+}
